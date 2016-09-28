2a856620f83b24a595bd4a7f6c797d3262ae5f2c
Elijah Schow <Elijah Schow>
2016-09-28 13:30:20 -0500

    Output "to" as a string instead of a list because the sendgrid API won't accept a list.

--- app.py
+++ app.py
@@ -16,22 +16,17 @@ def main(argv):
     team_data = readCSV( settings['team_file'] )
     this_round = readCSV( settings['round_file'] )
 
-    if __debug__:
-        print( team_data )
-        print( this_round )
-
     for row in this_round:
+        recipients = emailsGet(row, team_data)
         room = {
             'round' : settings['round'],
             'affirmative' : row['AFF'],
             'negative' : row['NEG'],
-            'to' : emailsGet(row, team_data)
+            'to' : recipients
         }
 
     print(room)
-
     q=raw_input("press close to exit")
-
     return 0
 
 """Load a CSV file into an array of dictionaries"""
@@ -41,10 +36,9 @@ def readCSV(file_name):
         reader = csv.DictReader(file)
         for row in reader:
             result.append(row)
-    if __debug__: print(result)
     return result
 
-"""..."""
+"""Return a list of email addresses for everyone in a room."""
 def emailsGet(room, team_data):
     emails = []
     try:
@@ -55,20 +49,33 @@ def emailsGet(room, team_data):
         #TODO: add an error log message
 
     for row in team_data:
-        #All the addresses go into the same array, so we don't care which team matched
+        """ It doesn't matter which team matches because all the addresses go
+        into the same array."""
         if row['Team']==affirmative or row['Team']==negative:
             if 'Email 1' in row:
-                #I used nested if statements instead of an "and" operator because calling row['Email X'] before verifying its existance could throw a Key Error. I can't use a try/except block because I want to check for the second e-mail address even if the first one failes.
+                """Nested if statements are used instead of an "and" operator
+                because calling row['Email X'] before verifying its existance
+                could throw a Key Error. Try/except can't be used because the
+                application should check for the second e-mail address even if
+                the first one doesn't exist."""
                 if row['Email 1']:
                     emails.append( row['Email 1'] )
             if 'Email 2' in row:
                 if row['Email 2']:
                     emails.append( row['Email 2'] )
+    return concatenate(emails)
 
-    if __debug__:
-        print(emails)
-
-    return emails
+def concatenate( items ):
+    result = ''
+    first = True
+    for item in items:
+        #Don't leave a delimiter dangling at the beginning or end
+        if not first:
+            result += ", "
+        else:
+            first = False
+        result += str(item)
+    return result
 
 if __name__ == "__main__":
     main(sys.argv)
