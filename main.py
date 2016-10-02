6d97297e5139f1825222958d7494e8fe7b13cf69
Elijah Schow <Elijah Schow>
2016-10-01 21:27:50 -0500

    Fix sendgrid and clean up the code. NOTE: DO NOT name a .py file with the same name as a library (e.g. 'sendgrid.py'). It will confuse python.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import csv
 import os
+import sendgrid
+from sendgrid.helpers.mail import *
 
+
+"""Settings"""
+sg = sendgrid.SendGridAPIClient(apikey='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')
 settings = {
     'from' : "ziggyonlinedebate@gmail.com",
     'subject': "Ziggy Online Debate",
@@ -11,23 +16,26 @@ settings = {
     'team_file' : 'data/Team Data.csv'
 }
 
+
 """Main Entry Point"""
 def main(argv):
     team_data = readCSV( settings['team_file'] )
     this_round = readCSV( settings['round_file'] )
+    if __debug__: print(this_round)
 
     for row in this_round:
-        recipients = emailsGet(row, team_data)
-        room = {
-            'round' : settings['round'],
-            'affirmative' : row['AFF'],
-            'negative' : row['NEG'],
-            'to' : recipients
-        }
+        recipients = recipientsGet(row, team_data)
+        message = messagePrepare(row, settings)
+        mail = Mail( Email(settings['from']), settings['subject'],\
+                     Email(recipients), Content('text/html', message ) )
+        response = sg.client.mail.send.post(request_body=mail.get())
+        if __debug__:
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
 
-    print(room)
-    q=raw_input("press close to exit")
     return 0
+
 
 """Load a CSV file into an array of dictionaries"""
 def readCSV(file_name):
@@ -38,8 +46,9 @@ def readCSV(file_name):
             result.append(row)
     return result
 
+
 """Return a list of email addresses for everyone in a room."""
-def emailsGet(room, team_data):
+def recipientsGet(room, team_data):
     emails = []
     try:
         affirmative = room['AFF']
@@ -65,6 +74,8 @@ def emailsGet(room, team_data):
                     emails.append( row['Email 2'] )
     return concatenate(emails)
 
+
+"""Convert a list into a comma-separated string"""
 def concatenate( items ):
     result = ''
     first = True
@@ -77,5 +88,19 @@ def concatenate( items ):
         result += str(item)
     return result
 
+
+"""Build the email's body"""
+def messagePrepare(row, settings):
+    return '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body>\
+                    <p>Hello,</p>\
+                    <p>Your debate round %s pairing is as follows: \
+                    Affirmative %s vs. Negative %s.</p>\
+                    <p>%s</p>\
+                </body></html>'\
+                % (settings['round'], row['AFF'],\
+                row['NEG'], settings['information'])
+
+
+"""..."""
 if __name__ == "__main__":
     main(sys.argv)
