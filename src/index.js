da10b447568e303cf9fb6b0cb616680288da7e71
Elijah Schow <elijah.schow@gmail.com>
2018-04-09 19:49:00 -0500

    Improve metadata

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ let mainWindow;
 
 const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+  mainWindow = new BrowserWindow();
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
